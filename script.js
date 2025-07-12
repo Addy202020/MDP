57608e502fa7e6d36efa28a56545cb6523f08eee
Aditya Mitra <Adityamitra2016@outlook.com>
2025-07-12 19:02:24 +0530

    up

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ document.querySelector('.brand-name').addEventListener('click', () => {
 
 // Folder where images are stored
 const folderPath = 'images';
-const totalImages = 1014;
+const totalImages = 999;
 
 // Generate image file names
 const imageNames = Array.from({ length: totalImages }, (_, i) => `image${i + 1}.webp`);
