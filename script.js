22140befacfc3fffe00387bd7b1cfc3bff252d7c
Aditya Mitra <Adityamitra2016@outlook.com>
2025-07-30 18:03:47 +0530

    Minor Fix

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ document.querySelector('.brand-name').addEventListener('click', () => {
 
 // Folder where images are stored
 const folderPath = 'images';
-const totalImages = 999;
+const totalImages = 1015;
 
 // Generate image file names
 const imageNames = Array.from({ length: totalImages }, (_, i) => `image${i + 1}.webp`);
---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const cacheName = 'mdp-cache-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/assets/logo.svg',
+  '/assets/logo-192.png',
+  '/assets/logo-512.png',
+  // Add other critical assets
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
